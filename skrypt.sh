6ad462bc44a2e907b3047d12b4ac8349e316ad81
Wiktor Szostak <wiktorszostak6@gmail.com>
2024-06-30 07:27:11 +0200

    dodanie funkcji --logs

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,21 @@
 if [ "$1" == "--date" ] then
     current_date=$(date +"%Y-%m-%d")
     echo "Dzisiejsza data: $current_date"
+elif [ "$1" == "--logs" ]; then
+    if [ -z "$2" ]; then
+        num_logs=100
+    else
+        num_logs=$2
+    fi
+
+    if ! [[ "$num_logs" =~ ^[0-9]+$ ]]; then
+        echo "Błąd: Podany argument nie jest liczbą całkowitą."
+        exit 1
+    fi
+
+    for (( i=1; i<=$num_logs; i++ )); do
+        log_file="log$i.txt"
+        echo "Nazwa pliku: $log_file" > "$log_file"
+        echo "Nazwa skryptu: skrypt.sh" >> "$log_file"
+        echo "Data utworzenia: $(date +"%Y-%m-%d %H:%M:%S")" >> "$log_file"
+    done
